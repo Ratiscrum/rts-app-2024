a166ca008c7f484d9898dfa3d1dea52058023d59
Raffael Di Pietro <maxou4170@gmail.com>
2024-12-06 00:49:30 +0100

    feat(ocean): create ocean background component

--- app/sea/page.tsx
+++ app/sea/page.tsx
@@ -1,10 +1,10 @@
-import OceanBackground from "@/components/ocean/ocean";
+import OceanBackground from '@/components/ocean/ocean';
 
 export default function SeaPage() {
   return (
     <div className={'flex flex-col'}>
       <h1>Sea page</h1>
-        <OceanBackground></OceanBackground>
+      <OceanBackground></OceanBackground>
     </div>
   );
 }
