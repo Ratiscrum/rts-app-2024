2993d27ab1b80b902f4da1d54039d10ca874baa9
Franck <gutmann.franck@outlook.fr>
2024-12-06 03:39:19 +0100

    fix: connard de linter

--- app/(game)/layout.tsx
+++ app/(game)/layout.tsx
@@ -5,7 +5,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <main>
       <Header className="lg:fixed" />
-      <div>{children}</div>
+      {children}
       <Footer />
     </main>
   );
