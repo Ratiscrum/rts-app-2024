c339041948f3c8514f381faed95d360ba8f31f86
Franck <gutmann.franck@outlook.fr>
2024-12-06 04:09:50 +0100

    fix scroll

--- app/(game)/layout.tsx
+++ app/(game)/layout.tsx
@@ -3,7 +3,7 @@ import Header from '@/components/layouts/header';
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <main className="flex w-full flex-col">
-      <Header className="lg:fixed" />
+      <Header className="md:fixed" />
       {children}
       {/* <Footer /> */}
     </main>
