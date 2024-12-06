4aba83cbe9c4839d7b0a1113143293482b2ac07d
Franck <gutmann.franck@outlook.fr>
2024-12-06 06:52:58 +0100

    disable fieifdz

--- components/content.tsx
+++ components/content.tsx
@@ -2,7 +2,7 @@
 
 import { MDXRemote } from 'next-mdx-remote';
 
-export default function Content({ mdx }: { mdx: object }): JSX.Element {
+export default function Content({ mdx }: { mdx: any }): JSX.Element {
   return (
     <div className="flex flex-col gap-2 text-sm text-foreground">
       <MDXRemote {...mdx} />
