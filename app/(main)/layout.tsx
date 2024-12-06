bc90dd717bf90a080fa19081fe8f6439b3e21de2
Franck <gutmann.franck@outlook.fr>
2024-12-06 02:31:42 +0100

    fix: global

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from 'next';
-import './globals.css';
 import Header from '@/components/layouts/header';
 import Footer from '@/components/layouts/footer';
 import { appName, appUrl, description } from '../manifest';
--- models/organs.type.ts
+++ models/organs.type.ts
@@ -1,3 +1,22 @@
-export type Organs = "heart" | "eyes" | "brain" | "rein" | 'nerves' | "bone" | "sweat" | "intestin" | "tissue"
+export type Organs =
+  | 'heart'
+  | 'eyes'
+  | 'brain'
+  | 'rein'
+  | 'nerves'
+  | 'bone'
+  | 'sweat'
+  | 'intestin'
+  | 'tissue';
 
-export const organs = ["heart", "eyes", "brain", "rein", 'nerves', "bone", "sweat", "intestin", "tissue"]
+export const organs = [
+  'heart',
+  'eyes',
+  'brain',
+  'rein',
+  'nerves',
+  'bone',
+  'sweat',
+  'intestin',
+  'tissue',
+];
